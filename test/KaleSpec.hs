63799b8545c8c075b561caed6a7bc414a756445b
Nikhil Addleman <nik.addleman@gmail.com>
2017-11-17 15:22:35 -0800

    Add tests for some other functions
    All of the tests pass but the correctness of the specs should be
    checked.

--- test/KaleSpec.hs
+++ test/KaleSpec.hs
@@ -24,6 +24,8 @@ spec = do
             casify "HelloWorld" `shouldBe` "Hello_World"
         it "acts strange with many caps letters" $
             casify "HTTPWorker" `shouldBe` "H_T_T_P_Worker"
+        it "acts strange with initial lowercase" $
+            casify "lowHigh" `shouldBe` "l_o_w_High"
 
     describe "decs" $ do
         it "roughly parses declarations" $ do
@@ -50,6 +52,33 @@ spec = do
                     [ "data Foo = Foo { fooName :: String}"
                     ]
 
+    describe "mkCommandSum" $ do
+        it "is empty on empty task lists" $
+            mkCommandSum [] `shouldBe` ""
+        it "lists tasktnames properly" $
+            mkCommandSum [task0, task1] 
+                `shouldBe`
+                    unwords ["data Command = Name|Other_Name deriving (Eq,",
+                        "Show, Read, Generic, ParseRecord)"]
+
+    describe "mkTask" $ do
+        it "is Nothing for taskArgs when fileContent is empty" $
+            mkTask "" "OtherName" "Module1.foo" `shouldBe` task1
+
+    describe "pathToModule" $ do
+        it "parses directories and file extensions" $
+            pathToModule "foo/bar.baz" == "Bar" `shouldBe` True
+
+    describe "importList" $ do
+        it "pulls out taskModules and splits into newlines" $
+            importList [task0, task1] `shouldBe`
+                unlines [
+                        "import qualified Module.fooTask",
+                        "import qualified Module1.fooTask"
+                        ]
+    
+                    
+                    
 decs0 :: String
 decs0 = unlines
     [ "module Foo where"
@@ -82,3 +111,9 @@ decs3 = unlines
     , " fooName :: String"
     , "}"
     ]
+
+task0 :: Task
+task0 = Task "Module.foo" (Just "args0 args1") "Name"
+
+task1 :: Task
+task1 = Task "Module1.foo" Nothing "Other_Name"
