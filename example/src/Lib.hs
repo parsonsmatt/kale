004720ad819f2025aa31628c0437e417c7196822
Colin Wahl <colin.t.wahl@gmail.com>
2017-11-20 10:33:50 -0500

    merge master

--- example/src/Lib.hs
+++ example/src/Lib.hs
@@ -1,1 +1,23 @@
-{-# OPTIONS_GHC -F -pgmF kale #-}
+{-# LINE 1 "./example/src/Lib" #-}
+{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
+
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib where
+
+import Kale.Discover
+import qualified Lib.BarTask
+import qualified Lib.FooTask
+
+
+data Command = Bar|Foo deriving (Eq, Show, Read, Generic, ParseRecord)
+
+kaleMain :: IO ()
+kaleMain = do
+  cmd <- getRecord "kale-discovery"
+  case (cmd :: Command) of
+    Bar -> Lib.BarTask.task
+    Foo -> Lib.FooTask.task
+
